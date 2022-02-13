2a4e5c0022b75e281dac0faa185bd92c6b68e8b5
Abdinajib-idle <47075793+Abdinajib-idle@users.noreply.github.com>
2022-02-12 22:26:48 -0800

    connected it to local database

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const { Pool } = require('pg');
+var pool;
+pool = new Pool({
+  connectionString: 'postgres://postgres:Postgress@localhost/Recs'
+})
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app =express()
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.get('/', (req, res) => res.render('pages/index'))
+  app.get('/database', (req, res) => {
+    var getusersQuery=`SELECT FROM Rectangles`;
+    pool.query(getUsersQuery, (error,result) => {
+      if (error)
+        res.end(error);
+      var results = {'rows':result.rows}
+     res.render('pages/db', results);
+    })
+  });
+
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
