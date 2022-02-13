6943aafd509f003cd6326d54aedbd53428cf89aa
Abdinajib-idle <47075793+Abdinajib-idle@users.noreply.github.com>
2022-02-13 00:15:26 -0800

    added get database

--- index.js
+++ index.js
@@ -12,14 +12,30 @@ var app =express()
   app.set('views', path.join(__dirname, 'views'))
   app.set('view engine', 'ejs')
   app.get('/', (req, res) => res.render('pages/index'))
-  app.get('/database', (req, res) => {
-    var getusersQuery=`SELECT FROM Rectangles`;
+  app.get('/database', async (req,res)=>{
+    /*
+    var getUsersQuery = `SELECT * FROM users`;
     pool.query(getUsersQuery, (error,result) => {
-      if (error)
-        res.end(error);
-      var results = {'rows':result.rows}
-     res.render('pages/db', results);
+        if (error) {
+            res.end(error)
+        }
+        else {
+            // data ?
+            var data = { results : result.rows };
+            res.render('pages/db', data);
+        }
     })
-  });
+    */
+   try {
+       const result = await pool.query(`SELECT * FROM users`);
+       const data = { results : result.rows };
+       res.render('pages/db', data);
+   }
+   catch (error) {
+        res.end(error);
+   }
+    
+})
+
 
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
