d6e9337eb8be38a007bd16fbbc76d80710547f27
Abdinajib-idle <47075793+Abdinajib-idle@users.noreply.github.com>
2022-02-13 16:17:37 -0800

    sjkh

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+
+var app = express()
+
 const { Pool } = require('pg');
-var pool;
-pool = new Pool({
-  connectionString:/*process.env.DATABASE_URL ||*/ "postgres://postgres:Postgress@localhost/Recs"
+var pool = new Pool({
+    connectionString: process.env.DATABASE_URL || "postgres://postgres:Postgress@localhost/Recs"
 })
-
-var app =express()
-  app.use(express.static(path.join(__dirname, 'public')))
-  app.set('views', path.join(__dirname, 'views'))
-  app.set('view engine', 'ejs')
-  app.get('/', (req, res) => res.render('pages/index'))
   
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
-  app.get('/database',(req,res)=>{
-    var getUsersQuery = `SELECT * FROM rectangles`;
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/database', async (req,res)=>{
+    /*
+    var getUsersQuery = `SELECT * FROM users`;
     pool.query(getUsersQuery, (error,result) => {
         if (error) {
             res.end(error)
@@ -26,10 +29,16 @@ var app =express()
             res.render('pages/db', data);
         }
     })
-
-     
- })
- 
-
+    */
+   try {
+       const result = await pool.query(`SELECT * FROM users`);
+       const data = { results : result.rows };
+       res.render('pages/db', data);
+   }
+   catch (error) {
+        res.end(error);
+   }
+    
+})
 
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
